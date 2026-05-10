97cb15e7bcc908b2d0a6fe5fb3103ae8d70b6114
ian <ixnovakosky@gmail.com>
2026-05-10 20:31:34 -0300

    Updating AgendaPBuffer

--- Agenda-PBuffer/Agenda.c
+++ Agenda-PBuffer/Agenda.c
@@ -2,256 +2,272 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NOME_MAX 50
+#define NAME_MAX 50
 #define EMAIL_MAX 50
-#define TAM_PESSOA (NOME_MAX + sizeof(int) + EMAIL_MAX)
 
-// Estrutura do buffer: qtd | opcao | tempNome | tempIdade | tempEmail | pessoas...
-#define AREA_FIXA (sizeof(int)*2 + NOME_MAX + sizeof(int) + EMAIL_MAX)
+// Buffer: count | option | peopleDataSize | tempName | tempAge | tempEmail | people...
+#define AREA_FIXA ( sizeof( int ) * 2 + sizeof( size_t ) + NAME_MAX + sizeof( int ) + EMAIL_MAX )
 
-void AddPessoa(void **ppBuffer);
-void RemoverPessoa(void **ppBuffer);
-void ListarPessoas(void *pBuffer);
-void BuscarPessoa(void *pBuffer);
+void AddPerson( void** ppBuffer );
+void DeletePerson( void** ppBuffer );
+void ListPeople( void* pBuffer );
+void PrintPerson( void* pBuffer );
 
 int main() {
 
-    void *pBuffer = malloc(AREA_FIXA);
-    if (!pBuffer) {
-        printf("Erro ao alocar memória!\n");
-        return 1;
-    }
+	void* pBuffer = malloc( AREA_FIXA );
+	if( !pBuffer ) {
+		printf( "Erro ao alocar memória!\n" );
+		return 1;
+	}
 
-    int *qtd = (int *)pBuffer;
-    *qtd = 0;
+	int* count = ( int* )pBuffer;
+	*count	   = 0;
 
-    int *opcao = (int *)((char*)pBuffer + sizeof(int));
+	int* option = ( int* )( ( char* )pBuffer + sizeof( int ) );
 
-    while (1) {
+	size_t* peopleDataSize = ( size_t* )( ( char* )pBuffer + sizeof( int ) * 2 );
+	*peopleDataSize		   = 0;
 
-        do {
-            printf("\n#------------------------#\n");
-            printf("#     Agenda pBuffer     #\n");
-            printf("#------------------------#\n");
-            printf("# 1 - Adicionar Pessoa   #\n");
-            printf("# 2 - Remover Pessoa     #\n");
-            printf("# 3 - Buscar Pessoa      #\n");
-            printf("# 4 - Listar Todas       #\n");
-            printf("# 5 - Sair               #\n");
-            printf("#------------------------#\n");
-            printf("Escolha uma opcao: ");
+	while( 1 ) {
 
-            if (scanf("%d", opcao) != 1) {
-                while (getchar() != '\n');
-                *opcao = 0;
-                continue;
-            }
+		do {
+			printf( "\n#------------------------#\n" );
+			printf( "#     Agenda pBuffer     #\n" );
+			printf( "#------------------------#\n" );
+			printf( "# 1 - Adicionar Pessoa   #\n" );
+			printf( "# 2 - Remover Pessoa     #\n" );
+			printf( "# 3 - Buscar Pessoa      #\n" );
+			printf( "# 4 - Listar Todas       #\n" );
+			printf( "# 5 - Sair               #\n" );
+			printf( "#------------------------#\n" );
+			printf( "Escolha uma opcao: " );
 
-            while (getchar() != '\n');
+			if( scanf( "%d", option ) != 1 ) {
+				while( getchar() != '\n' )
+					;
+				*option = 0;
+				continue;
+			}
 
-        } while (*opcao < 1 || *opcao > 5);
+			while( getchar() != '\n' )
+				;
 
-        switch (*opcao) {
-            case 1:
-                AddPessoa(&pBuffer);
-                // Atualizar ponteiros após realloc
-                qtd = (int *)pBuffer;
-                opcao = (int *)((char*)pBuffer + sizeof(int));
-                break;
+		} while( *option < 1 || *option > 5 );
 
-            case 2:
-                RemoverPessoa(&pBuffer);
-                // Atualizar ponteiros após realloc
-                qtd = (int *)pBuffer;
-                opcao = (int *)((char*)pBuffer + sizeof(int));
-                break;
+		switch( *option ) {
+		case 1:
+			AddPerson( &pBuffer );
+			// Atualizar ponteiros após realloc
+			count  = ( int* )pBuffer;
+			option = ( int* )( ( char* )pBuffer + sizeof( int ) );
+			break;
 
-            case 3:
-                BuscarPessoa(pBuffer);
-                break;
+		case 2:
+			DeletePerson( &pBuffer );
+			// Atualizar ponteiros após realloc
+			count  = ( int* )pBuffer;
+			option = ( int* )( ( char* )pBuffer + sizeof( int ) );
+			break;
 
-            case 4:
-                ListarPessoas(pBuffer);
-                break;
+		case 3:
+			PrintPerson( pBuffer );
+			break;
 
-            case 5:
-                printf("\nSaindo...\n");
-                free(pBuffer);
-                return 0;
+		case 4:
+			ListPeople( pBuffer );
+			break;
 
-            default:
-                printf("Opcao inválida.\n");
-        }
-    }
+		case 5:
+			printf( "\nSaindo...\n" );
+			free( pBuffer );
+			return 0;
 
-    return 0;
+		default:
+			printf( "Opcao inválida.\n" );
+		}
+	}
+
+	return 0;
 }
 
-void AddPessoa(void **ppBuffer) {
+void AddPerson( void** ppBuffer ) {
 
-    int *qtd = (int *)(*ppBuffer);
+	int* count = ( int* )( *ppBuffer );
 
-    // Área temporária no buffer para guardar os dados antes de salvar
-    char *tempNome  = (char*)(*ppBuffer) + sizeof(int)*2;
-    int  *tempIdade = (int *)(tempNome + NOME_MAX);
-    char *tempEmail = (char *)(tempIdade + 1);
+	// Área temporária no buffer para guardar os dados antes de salvar
+	char* tempName	= ( char* )( *ppBuffer ) + sizeof( int ) * 2 + sizeof( size_t );
+	int*  tempAge	= ( int* )( tempName + NAME_MAX );
+	char* tempEmail = ( char* )( tempAge + 1 );
 
-    printf("\nNome: ");
-    fgets(tempNome, NOME_MAX, stdin);
-    tempNome[strcspn(tempNome, "\n")] = '\0';
+	printf( "\nNome: " );
+	fgets( tempName, NAME_MAX, stdin );
+	tempName[strcspn( tempName, "\n" )] = '\0';
 
-    printf("Idade: ");
-    scanf("%d", tempIdade);
-    getchar();
+	printf( "Email: " );
+	fgets( tempEmail, EMAIL_MAX, stdin );
+	tempEmail[strcspn( tempEmail, "\n" )] = '\0';
 
-    printf("Email: ");
-    fgets(tempEmail, EMAIL_MAX, stdin);
-    tempEmail[strcspn(tempEmail, "\n")] = '\0';
+	printf( "Idade: " );
+	while( scanf( "%d", tempAge ) != 1 ) {
+		while( getchar() != '\n' )
+			;
+		printf( "Idade invalida. Digite um numero: " );
+	}
+	getchar();
 
-    // Aumentar o buffer para caber mais uma pessoa
-    *ppBuffer = realloc(*ppBuffer, AREA_FIXA + TAM_PESSOA * ((*qtd) + 1));
-    if (!(*ppBuffer)) {
-        printf("Erro ao realocar memória.\n");
-        exit(1);
-    }
+	// Aumentar o buffer para caber mais uma pessoa
+	size_t* peopleDataSize = ( size_t* )( ( char* )*ppBuffer + sizeof( int ) * 2 );
 
-    // Atualizar ponteiros após realloc (endereço pode ter mudado)
-    qtd = (int *)(*ppBuffer);
-    tempNome  = (char*)(*ppBuffer) + sizeof(int)*2;
-    tempIdade = (int *)(tempNome + NOME_MAX);
-    tempEmail = (char *)(tempIdade + 1);
+	char* people  = ( char* )( *ppBuffer ) + AREA_FIXA;
+	void* listEnd = ( char* )( *ppBuffer ) + AREA_FIXA + *peopleDataSize;
 
-    // Posição onde a nova pessoa será salva
-    char *novaPessoa = (char*)(*ppBuffer) + AREA_FIXA + TAM_PESSOA * (*qtd);
-    
-    memcpy(novaPessoa, tempNome, NOME_MAX);
-    memcpy(novaPessoa + NOME_MAX, tempIdade, sizeof(int));
-    memcpy(novaPessoa + NOME_MAX + sizeof(int), tempEmail, EMAIL_MAX);
+	while( people < ( char* )listEnd ) {
+		char* name	= ( char* )people + sizeof( int );
+		char* email = ( char* )( name + strlen( name ) + 1 );
+		char* next	= ( char* )( email + strlen( email ) + 1 );
 
-    (*qtd)++;
+		if( strcmp( email, tempEmail ) == 0 ) {
+			printf( "\nJa existe uma pessoa com este email!\n" );
+			return;
+		}
+		people = next;
+	}
 
-    printf("\nPessoa adicionada com sucesso!\n");
+	*ppBuffer = realloc( *ppBuffer, AREA_FIXA + *peopleDataSize + sizeof( int ) + strlen( tempName ) + 1 + strlen( tempEmail ) + 1 );
+
+	if( !( *ppBuffer ) ) {
+		printf( "Erro ao realocar memória.\n" );
+		exit( 1 );
+	}
+
+	// Atualizar ponteiros após realloc (endereço pode ter mudado)
+	count		   = ( int* )( *ppBuffer );
+	peopleDataSize = ( size_t* )( ( char* )*ppBuffer + sizeof( int ) * 2 );
+	tempName	   = ( char* )( *ppBuffer ) + sizeof( int ) * 2 + sizeof( size_t );
+	tempAge		   = ( int* )( tempName + NAME_MAX );
+	tempEmail	   = ( char* )( tempAge + 1 );
+
+	// Posição onde a nova pessoa será salva
+	char* newPerson = ( char* )( *ppBuffer ) + AREA_FIXA + *peopleDataSize;
+
+	memcpy( newPerson, tempAge, sizeof( int ) );
+	memcpy( newPerson + sizeof( int ), tempName, strlen( tempName ) + 1 );
+	memcpy( newPerson + sizeof( int ) + strlen( tempName ) + 1, tempEmail, strlen( tempEmail ) + 1 );
+
+	*peopleDataSize += sizeof( int ) + strlen( tempName ) + 1 + strlen( tempEmail ) + 1;
+
+	( *count )++;
+
+	printf( "\nPessoa adicionada com sucesso!\n" );
 }
 
-void RemoverPessoa(void **ppBuffer) {
-    
-    int *qtd = (int*)(*ppBuffer);
+void DeletePerson( void** ppBuffer ) {
+	int* count = ( int* )( *ppBuffer );
+	if( *count == 0 ) {
+		printf( "\nNenhuma pessoa cadastrada.\n" );
+		return;
+	}
 
-    if (*qtd == 0) {
-        printf("\nNenhuma pessoa cadastrada.\n");
-        return;
-    }
+	char* tempEmail = ( char* )( *ppBuffer ) + sizeof( int ) * 2 + sizeof( size_t ) + NAME_MAX + sizeof( int );
 
-    char *tempEmail = (char*)(*ppBuffer) + sizeof(int)*2 + NOME_MAX + sizeof(int);
+	printf( "\nDigite o email da pessoa a remover: " );
+	fgets( tempEmail, EMAIL_MAX, stdin );
+	tempEmail[strcspn( tempEmail, "\n" )] = '\0';
 
-    printf("\nDigite o email da pessoa a remover: ");
-    fgets(tempEmail, EMAIL_MAX, stdin);
-    tempEmail[strcspn(tempEmail, "\n")] = '\0';
+	size_t* peopleDataSize = ( size_t* )( ( char* )*ppBuffer + sizeof( int ) * 2 );
+	char*	people		   = ( char* )( *ppBuffer ) + AREA_FIXA;
+	void*	listEnd		   = ( char* )( *ppBuffer ) + AREA_FIXA + *peopleDataSize;
 
-    char *pessoa = (char*)(*ppBuffer) + AREA_FIXA;
+	while( people < ( char* )listEnd ) {
+		int*  age	= ( int* )people;
+		char* name	= ( char* )people + sizeof( int );
+		char* email = ( char* )( name + strlen( name ) + 1 );
+		char* next	= ( char* )( email + strlen( email ) + 1 );
 
-    int *i = (int*)malloc(sizeof(int));
-    for (*i = 0; *i < *qtd; (*i)++) {
-        
-        char *emailPessoa = (char*)(pessoa + NOME_MAX + sizeof(int));
+		if( strcmp( email, tempEmail ) == 0 ) {
+			printf( "\nPessoa encontrada e removida.\n" );
 
-        if (strcmp(emailPessoa, tempEmail) == 0) {
-            printf("\nPessoa encontrada e removida.\n");
+			size_t peopleSize = sizeof( int ) + strlen( name ) + 1 + strlen( email ) + 1;
 
-            // Mover todas as pessoas seguintes uma posição para trás
-            char *proxima = pessoa + TAM_PESSOA;
-            memmove(pessoa, proxima, (*qtd - *i - 1) * TAM_PESSOA);
+			memmove( people, next, ( char* )listEnd - ( char* )next );
 
-            (*qtd)--;
+			( *count )--;
+			*peopleDataSize -= peopleSize;
 
-            // Diminuir o tamanho do buffer
-            *ppBuffer = realloc(*ppBuffer, AREA_FIXA + (*qtd) * TAM_PESSOA);
-            if (!(*ppBuffer) && *qtd > 0) {
-                printf("Erro ao realocar memória.\n");
-                free(i);
-                return;
-            }
-
-            free(i);
-            return;
-        }
-
-        pessoa += TAM_PESSOA;
-    }
-
-    printf("\nPessoa nao encontrada.\n");
-    free(i);
+			*ppBuffer = realloc( *ppBuffer, AREA_FIXA + *peopleDataSize );
+			if( !( *ppBuffer ) && *peopleDataSize > 0 ) {
+				printf( "Erro ao realocar memória.\n" );
+				return;
+			}
+			return;
+		}
+		people = next;
+	}
+	printf( "\nPessoa nao encontrada.\n" );
 }
 
-void ListarPessoas(void *pBuffer) {
+void ListPeople( void* pBuffer ) {
+	int* count = ( int* )pBuffer;
 
-    int *qtd = (int*)pBuffer;
-    
-    if (*qtd == 0) {
-        printf("\nNenhuma pessoa cadastrada.\n");
-        return;
-    }
+	if( *count == 0 ) {
+		printf( "\nNenhuma pessoa cadastrada.\n" );
+		return;
+	}
 
-    printf("\n--- LISTA DE PESSOAS ---\n");
+	printf( "\n--- LISTA DE PESSOAS ---\n" );
 
-    char *pessoa = (char*)pBuffer + AREA_FIXA;
+	size_t* peopleDataSize = ( size_t* )( ( char* )pBuffer + sizeof( int ) * 2 );
+	char*	people		   = ( char* )pBuffer + AREA_FIXA;
+	void*	listEnd		   = ( char* )pBuffer + AREA_FIXA + *peopleDataSize;
 
-    int *i = (int*)malloc(sizeof(int));
-    for (*i = 0; *i < *qtd; (*i)++) {
+	while( people < ( char* )listEnd ) {
+		int*  age	= ( int* )people;
+		char* name	= ( char* )people + sizeof( int );
+		char* email = ( char* )( name + strlen( name ) + 1 );
+		char* next	= ( char* )( email + strlen( email ) + 1 );
 
-        char *nome  = pessoa;
-        int  *idade = (int*)(pessoa + NOME_MAX);
-        char *email = (char*)(pessoa + NOME_MAX + sizeof(int));
+		printf( "\nPessoa:\n" );
+		printf( "Nome : %s\n", name );
+		printf( "Idade: %d\n", *age );
+		printf( "Email: %s\n", email );
 
-        printf("\nPessoa %d:\n", (*i) + 1);
-        printf("Nome : %s\n", nome);
-        printf("Idade: %d\n", *idade);
-        printf("Email: %s\n", email);
-
-        pessoa += TAM_PESSOA;
-    }
-    
-    free(i);
+		people = next;
+	}
 }
 
-void BuscarPessoa(void *pBuffer) {
+void PrintPerson( void* pBuffer ) {
+	int* count = ( int* )pBuffer;
 
-    int *qtd = (int*)pBuffer;
+	if( *count == 0 ) {
+		printf( "\nNenhuma pessoa cadastrada.\n" );
+		return;
+	}
 
-    if (*qtd == 0) {
-        printf("\nNenhuma pessoa cadastrada.\n");
-        return;
-    }
+	char* tempEmail = ( char* )pBuffer + sizeof( int ) * 2 + sizeof( size_t ) + NAME_MAX + sizeof( int );
 
-    char *tempEmail = (char*)pBuffer + sizeof(int)*2 + NOME_MAX + sizeof(int);
+	printf( "\nDigite o email para buscar: " );
+	fgets( tempEmail, EMAIL_MAX, stdin );
+	tempEmail[strcspn( tempEmail, "\n" )] = '\0';
 
-    printf("\nDigite o email para buscar: ");
-    fgets(tempEmail, EMAIL_MAX, stdin);
-    tempEmail[strcspn(tempEmail, "\n")] = '\0';
+	size_t* peopleDataSize = ( size_t* )( ( char* )pBuffer + sizeof( int ) * 2 );
+	char*	people		   = ( char* )pBuffer + AREA_FIXA;
+	void*	listEnd		   = ( char* )pBuffer + AREA_FIXA + *peopleDataSize;
 
-    char *pessoa = (char*)pBuffer + AREA_FIXA;
+	while( people < ( char* )listEnd ) {
+		int*  age	= ( int* )people;
+		char* name	= ( char* )people + sizeof( int );
+		char* email = ( char* )( name + strlen( name ) + 1 );
+		char* next	= ( char* )( email + strlen( email ) + 1 );
 
-    int *i = (int*)malloc(sizeof(int));
-    for (*i = 0; *i < *qtd; (*i)++) {
-
-        char *nome = pessoa;
-        int *idade = (int*)(pessoa + NOME_MAX);
-        char *emailPessoa = (char*)(pessoa + NOME_MAX + sizeof(int));
-
-        if (strcmp(emailPessoa, tempEmail) == 0) {
-
-            printf("\n--- PESSOA ENCONTRADA ---\n");
-            printf("\nNome : %s\n", nome);
-            printf("Idade: %d\n", *idade);
-            printf("Email: %s\n", emailPessoa);
-            
-            free(i);
-            return;
-        }
-
-        pessoa += TAM_PESSOA;
-    }
-
-    printf("\nPessoa nao encontrada.\n");
-    free(i);
+		if( strcmp( email, tempEmail ) == 0 ) {
+			printf( "\n--- PESSOA ENCONTRADA ---\n" );
+			printf( "\nNome : %s\n", name );
+			printf( "Idade: %d\n", *age );
+			printf( "Email: %s\n", email );
+			return;
+		}
+		people = next;
+	}
+	printf( "\nPessoa nao encontrada.\n" );
 }
---
+++ ProgDesc/ListaC05-1.c
@@ -0,0 +1,15 @@
+/*Faca um programa que entao leia uma string e a imprima*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char nome[10];
+
+    printf("Digite uma palavra: ");
+    scanf("%d", &nome);
+
+    printf("%d", nome);
+
+    return 0;
+}
